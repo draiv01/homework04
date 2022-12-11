8ac40e99836191f64388b94890760574f4752f85
Alexandr Makarov <makarena2009@yandex.ru>
2022-12-11 16:09:39 +0300

    homework4

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// int Stepen(int a, int b)
+// {
+//     int st = 1;
+//     for(int i = 1; i<=b; i++)
+//         st = st * a;
+//     return st;
+// }
+
+// Console.Write("Enter your number a: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter your number b: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// int st = Stepen(a, b);
+// Console.WriteLine("Ответ: " + st);
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+// int Counter(int a)
+// {
+//     if (a < 10)
+//     return a;
+//         int dig = a % 10;
+//         int next = a /10;
+//     return dig + Counter(next);
+    
+// }
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int res = Counter(num);
+
+// Console.WriteLine($"Количество цифр числа {num} равно {res}");
+
